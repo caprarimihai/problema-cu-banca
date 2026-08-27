ba5f1baf31f31f22ad882bd910b29326195a4cb9
caprarimihai <caprarimihail@gmail.com>
2026-08-27 13:09:01 +0300

    initial commit

---
+++ src/Metode.java
@@ -0,0 +1,19 @@
+public class Metode {
+    static void main(String[] args) {
+        Metode maib = new Metode();
+        maib.name= "maib";
+        maib.procent= 7.7;
+        maib.numDeClient=1000;
+        Metode victoria= new Metode();
+        victoria.name= "Victoria";
+        victoria.numDeClient=2000;
+        victoria.procent=7.8;
+
+        System.out.println( "NUme banca"+maib.name+ "Procentul acordat"+maib.procent+  "NUmarul de clienti"+maib.numDeClient);
+    }
+
+    String name;
+    double procent;
+    int numDeClient;
+
+}
